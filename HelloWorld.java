d3e6c73f43379a4774219ded3ab876a3388ae3c3
sasi-selvaraj <sasiselvaraj1112@gmail.com>
2024-05-27 11:55:00 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
